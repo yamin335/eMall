4113811bec995045e41f4442aaf0c7b52d7ac261
md.yamin <myamin335@gmail.com>
2020-09-21 21:47:23 +0600

    Night push by YAMIN -- 21 September, 2020, 9:47 PM

--- app/src/main/java/com/rtchubs/engineerbooks/ui/login/ViewPagerViewModel.kt
+++ app/src/main/java/com/rtchubs/engineerbooks/ui/login/ViewPagerViewModel.kt
@@ -8,11 +8,11 @@ import javax.inject.Inject
 class ViewPagerViewModel @Inject constructor(private val preferencesHelper: PreferencesHelper) :
     ViewModel() {
     val slideDataList = listOf<SlideData>(
-        SlideData(R.drawable.slider_image_1, "Transfer your Money", "Easy, Fast and Secure Way"),
-        SlideData(R.drawable.slider_image_1, "Transfer your Money", "Easy, Fast and Secure Way"),
-        SlideData(R.drawable.slider_image_1, "Transfer your Money", "Easy, Fast and Secure Way"),
-        SlideData(R.drawable.slider_image_1, "Transfer your Money", "Easy, Fast and Secure Way"),
-        SlideData(R.drawable.slider_image_1, "Transfer your Money", "Easy, Fast and Secure Way")
+        SlideData(R.drawable.slider_image_1, "Learn with online tutor", "A Complete Solution for e-learning"),
+        SlideData(R.drawable.slider_image_1, "Learn with online tutor", "A Complete Solution for e-learning"),
+        SlideData(R.drawable.slider_image_1, "Learn with online tutor", "A Complete Solution for e-learning"),
+        SlideData(R.drawable.slider_image_1, "Learn with online tutor", "A Complete Solution for e-learning"),
+        SlideData(R.drawable.slider_image_1, "Learn with online tutor", "A Complete Solution for e-learning")
     )
 
     inner class SlideData(
